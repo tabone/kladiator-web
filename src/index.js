ce964d0a1634f881cd87bd0bae03f9a8f2da92ac
Luca Tabone <tabone@users.noreply.github.com>
2018-05-01 18:04:26 +0200

    Clean up boilerplate code
    Clean up `create-react-app` boilerplate code:
    - Removal of redundant files.
    - Rename of `App` component to `Root`.
    - Enable ReactJS Strict Mode throughout all the app.
    - Install missing `ajv` peer dependency.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './Root'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<React.StrictMode><Root /></React.StrictMode>,
+  document.getElementById('root'))
