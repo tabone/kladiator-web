32230e2858abd239d5a1ddaf62de2d30d157c56c
Luca Tabone <tabone@users.noreply.github.com>
2018-05-06 10:51:58 +0200

    Setup Material UI
    - Added Material UI as dependency.
    - Roboto font & Material Design Icons are now being requested.
    - Setup app's Material UI theme

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,21 @@
 import React, { PureComponent } from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import CssBaseline from 'material-ui/CssBaseline'
+import red from 'material-ui/colors/red'
+import teal from 'material-ui/colors/teal'
+import purple from 'material-ui/colors/purple'
 import RootContainer from './RootContainer'
 import reducers from './reducers/index'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: purple,
+    error: red
+  }
+})
 
 export default class Main extends PureComponent {
   constructor (props) {
@@ -13,9 +26,12 @@ export default class Main extends PureComponent {
   render () {
     return (
       <Provider store={this.store}>
-        <React.StrictMode>
-          <RootContainer />
-        </React.StrictMode>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <React.StrictMode>
+            <RootContainer />
+          </React.StrictMode>
+        </MuiThemeProvider>
       </Provider>
     )
   }
--- src/Main.test.js
+++ src/Main.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import CssBaseline from 'material-ui/CssBaseline'
+import { MuiThemeProvider } from 'material-ui/styles'
 import { shallow } from 'enzyme'
 import Main from './Main'
 import RootContainer from './RootContainer'
 
 describe('Main component', () => {
-  it('should render the Root Container Component within the Redux Provider', () => {
+  it('should render the Root Container Component within the Redux & Theme Provider', () => {
     const wrapper = shallow(<Main />)
     expect(wrapper.is(Provider)).toBe(true)
-    expect(wrapper.find(RootContainer).length).toBe(1)
+
+    const themeWrapper = wrapper.find(MuiThemeProvider)
+    expect(themeWrapper.length).toBe(1)
+    expect(themeWrapper.find(CssBaseline).length).toBe(1)
+    expect(themeWrapper.find(RootContainer).length).toBe(1)
   })
 })
