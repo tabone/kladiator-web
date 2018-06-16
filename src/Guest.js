b11ef192b1ff6d5c6cf3d26afa0dfc7454a2e72e
Luca Tabone <taboneluca3@gmail.com>
2018-06-16 12:44:43 +0200

    Setup routes for Guest pages

--- src/Guest.js
+++ src/Guest.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import LoginContainer from './LoginContainer'
+import RegisterContainer from './RegisterContainer'
+import ForgotPasswordContainer from './ForgotPasswordContainer'
 
 export default class Guest extends Component {
   render () {
-    return (<span>Guest</span>)
+    return (
+      <Switch>
+        <Route path='/' exact={true} component={ LoginContainer } />
+        <Route path='/register' component={ RegisterContainer } />
+        <Route path='/forgot-password' component={ ForgotPasswordContainer } />
+      </Switch>
+    )
   }
 }
--- src/Guest.test.js
+++ src/Guest.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import ReactDOM from 'react-dom'
 import Guest from './Guest'
 
 describe('Guest Component', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Guest />, div)
-    ReactDOM.unmountComponentAtNode(div)
+    shallow(<Guest />)
   })
 })
