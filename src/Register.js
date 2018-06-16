07c66b7ddc5dd031a2e18f09501684a8f96ca6ef
Luca Tabone <taboneluca3@gmail.com>
2018-06-16 13:20:39 +0200

    Create component for Register page

---
+++ src/Register.js
@@ -0,0 +1,171 @@
+import React, { PureComponent } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { setupHandlers } from './utils/index'
+import EmailTextField from './EmailTextField'
+import UsernameTextField from './UsernameTextField'
+import PasswordTextField from './PasswordTextField'
+import PasswordConfirmationTextField from './PasswordConfirmationTextField'
+
+const styles = () => {
+  return {
+    appRegister: {
+      padding: '18px',
+      minHeight: '100vh'
+    },
+    appRegisterAvatar: {
+      textAlign: 'center'
+    }
+  }
+}
+
+const fieldValidityMapper = {
+  email: 'isEmailValid',
+  username: 'isUsernameValid',
+  password: 'isPasswordValid',
+  passwordConfirmation: 'isPasswordConfirmationValid'
+}
+
+class Register extends PureComponent {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+      isEmailValid: false,
+      isUsernameValid: false,
+      isPasswordValid: false,
+      isPasswordConfirmationValid: false
+    }
+
+    this.onValidityChange = setupHandlers([
+      'email',
+      'username',
+      'password',
+      'passwordConfirmation'
+    ], this.onValidityChange, this)
+
+    this.handleTextFieldChange = setupHandlers([
+      'email',
+      'username',
+      'password',
+      'passwordConfirmation'
+    ], this.handleTextFieldChange, this)
+  }
+
+  render () {
+    const { classes } = this.props
+
+    return (
+      <Grid container={true}
+        direction='column'
+        justify='space-between'
+        className={classes.appRegister}>
+        <Grid item={true} className={classes.appRegisterAvatar}>
+          <img src='images/happy.png' width='200' alt='Happy Kladiator' />
+        </Grid>
+
+        <Grid item={true}>
+          <Typography variant='title' align='center'>Join Our Community!</Typography>
+        </Grid>
+
+        <Grid container={true} item={true} direction='column' spacing={16}>
+          <Grid item={true}>
+            <UsernameTextField
+              margin='none'
+              required={true}
+              fullWidth={true}
+              autoFocus={true}
+              label='Username'
+              value={this.state.username}
+              className='app-register__username-textfield'
+              onChange={this.handleTextFieldChange.username}
+              onValidityChange={this.onValidityChange.username} />
+          </Grid>
+
+          <Grid item={true}>
+            <EmailTextField
+              margin='none'
+              required={true}
+              fullWidth={true}
+              label='Email'
+              value={this.state.email}
+              className='app-register__email-textfield'
+              onChange={this.handleTextFieldChange.email}
+              onValidityChange={this.onValidityChange.email} />
+          </Grid>
+
+          <Grid item={true}>
+            <PasswordTextField
+              margin='none'
+              required={true}
+              fullWidth={true}
+              label='Password'
+              value={this.state.password}
+              className='app-register__password-textfield'
+              onChange={this.handleTextFieldChange.password}
+              onValidityChange={this.onValidityChange.password} />
+          </Grid>
+
+          <Grid item={true}>
+            <PasswordConfirmationTextField
+              margin='none'
+              required={true}
+              fullWidth={true}
+              label='Retype password'
+              password={this.state.password}
+              value={this.state.passwordConfirmation}
+              className='app-register__password-confirmation-textfield'
+              onChange={this.handleTextFieldChange.passwordConfirmation}
+              onValidityChange={this.onValidityChange.passwordConfirmation} />
+          </Grid>
+
+          <Grid item={true}>
+            <Button
+              variant='raised'
+              color='primary'
+              fullWidth={true}
+              className='app-register__submit-button'
+              disabled={this.isFormSubmittable() === false}>
+              Register
+            </Button>
+          </Grid>
+
+          <Grid item={true}>
+            <Button
+              to='/'
+              fullWidth={true}
+              component={Link}
+              className='app-register__to-login-button'>
+              Back to login
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
+  }
+
+  handleTextFieldChange (field, e) {
+    this.setState({ [field]: e.target.value })
+  }
+
+  onValidityChange (field, isValid) {
+    this.setState({ [ fieldValidityMapper[field] ]: isValid })
+  }
+
+  isFormSubmittable () {
+    return this.state.isEmailValid &&
+      this.state.isUsernameValid &&
+      this.state.isPasswordValid &&
+      this.state.isPasswordConfirmationValid
+  }
+}
+
+
+export default withStyles(styles)(Register)
---
+++ src/Register.test.js
@@ -0,0 +1,203 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Register from './Register'
+
+function getElementByClassName (wrapper, className) {
+  return wrapper.find(className)
+}
+
+function getTextField (wrapper, textField) {
+  return getElementByClassName(wrapper,
+    `.app-register__${textField}-textfield`)
+}
+
+function getButton (wrapper, button) {
+  return getElementByClassName(wrapper,
+    `.app-register__${button}-button`)
+}
+
+describe('Register Component', () => {
+  describe('Rendering the component', () => {
+    let wrapper = null
+
+    beforeEach(() => {
+      wrapper = shallow(<Register />).dive()
+    })
+
+    it('should display an empty username TextField', () => {
+      expect(getTextField(wrapper, 'username').props().value).toBe('')
+    })
+
+    it('should display an empty email TextField', () => {
+      expect(getTextField(wrapper, 'email').props().value).toBe('')
+    })
+
+    it('should display an empty password TextField', () => {
+      expect(getTextField(wrapper, 'password').props().value).toBe('')
+    })
+
+    it('should display an empty password confirmation TextField', () => {
+      expect(getTextField(wrapper, 'password-confirmation').props().value)
+        .toBe('')
+    })
+
+    it('should not allow the user to submit the form', () => {
+      expect(getButton(wrapper, 'submit').props().disabled).toBe(true)
+    })
+  })
+
+  describe('Entering a username', () => {
+    let value = null
+    let wrapper = null
+
+    beforeEach(() => {
+      value = 'username-value'
+      wrapper = shallow(<Register />).dive()
+      getTextField(wrapper, 'username').props().onChange({ target: { value } })
+      wrapper.update()
+    })
+
+    it('should store the entered username', () => {
+      expect(wrapper.state('username')).toBe(value)
+    })
+
+    it('should display the entered username in the username TextField', () => {
+      expect(getTextField(wrapper, 'username').props().value).toBe(value)
+    })
+  })
+
+  describe('Entering a email', () => {
+    let value = null
+    let wrapper = null
+
+    beforeEach(() => {
+      value = 'email-value'
+      wrapper = shallow(<Register />).dive()
+      getTextField(wrapper, 'email').props().onChange({ target: { value } })
+      wrapper.update()
+    })
+
+    it('should store the entered email', () => {
+      expect(wrapper.state('email')).toBe(value)
+    })
+
+    it('should display the entered email in the email TextField', () => {
+      expect(getTextField(wrapper, 'email').props().value).toBe(value)
+    })
+  })
+
+  describe('Entering a password', () => {
+    let value = null
+    let wrapper = null
+
+    beforeEach(() => {
+      value = 'password-value'
+      wrapper = shallow(<Register />).dive()
+      getTextField(wrapper, 'password').props().onChange({ target: { value } })
+      wrapper.update()
+    })
+
+    it('should store the entered password', () => {
+      expect(wrapper.state('password')).toBe(value)
+    })
+
+    it('should display the entered password in the password TextField', () => {
+      expect(getTextField(wrapper, 'password').props().value).toBe(value)
+    })
+  })
+
+  describe('Entering a password confirmation', () => {
+    let value = null
+    let wrapper = null
+
+    beforeEach(() => {
+      value = 'password-confirmation-value'
+      wrapper = shallow(<Register />).dive()
+      getTextField(wrapper, 'password-confirmation').props().onChange({
+        target: { value }
+      })
+      wrapper.update()
+    })
+
+    it('should store the entered password confirmation', () => {
+      expect(wrapper.state('passwordConfirmation')).toBe(value)
+    })
+
+    it('should display the entered password in the password TextField', () => {
+      expect(getTextField(wrapper, 'password-confirmation').props().value)
+        .toBe(value)
+    })
+  })
+
+  describe('Entering valid details', () => {
+    let wrapper = null
+
+    beforeEach(() => {
+      wrapper = shallow(<Register />).dive()
+
+      ;[
+        'email',
+        'username',
+        'password',
+        'password-confirmation'
+      ].forEach(field => {
+        getTextField(wrapper, field).props().onValidityChange(true)
+      })
+
+      wrapper.update()
+    })
+
+    it('should allow the user to submit the form', () => {
+      expect(getButton(wrapper, 'submit').props().disabled).toBe(false)
+    })
+  })
+
+  describe('Entering invalid details', () => {
+    let wrapper = null
+
+    beforeEach(() => {
+      wrapper = shallow(<Register />).dive()
+    })
+
+    describe('when one of the fields is invalid', () => {
+      it('should not allow the user to submit the form', () => {
+        const fields = [
+          'email',
+          'username',
+          'password',
+          'password-confirmation'
+        ]
+
+        fields.forEach(invalidField => {
+          fields.forEach(field => {
+            getTextField(wrapper, field).props()
+              .onValidityChange(field === invalidField)
+          })
+
+          wrapper.update()
+
+          expect(getButton(wrapper, 'submit').props().disabled).toBe(true)
+        })
+      })
+    })
+
+    describe('when all the fields are invalid', () => {
+      beforeEach(() => {
+        ;[
+          'email',
+          'username',
+          'password',
+          'password-confirmation'
+        ].forEach(field => {
+          getTextField(wrapper, field).props().onValidityChange(false)
+        })
+
+        wrapper.update()
+      })
+
+      it('should not allow the user to submit the form', () => {
+        expect(getButton(wrapper, 'submit').props().disabled).toBe(true)
+      })
+    })
+  })
+})
