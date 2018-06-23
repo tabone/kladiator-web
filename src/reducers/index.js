c8a332b30e2c55cd4089dc1542e5893d4306286e
Luca Tabone <taboneluca3@gmail.com>
2018-06-23 09:52:06 +0200

    Include notification reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux'
 
 import user from './user'
 import session from './session'
+import notification from './notification'
 
 export default combineReducers({
-  user, session
+  user, session, notification
 })
