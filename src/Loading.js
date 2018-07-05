1f36761f73725b6c3597d19968de73c5a3844bdb
Luca Tabone <tabone@users.noreply.github.com>
2018-07-05 20:24:56 +0200

    Stylize Loading Page

--- src/Loading.js
+++ src/Loading.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
-export default class Loading extends Component {
-  render () {
-    return (<span>This is loading :)</span>)
+const styles = theme => {
+  return {
+    appLoading: {
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundColor: theme.palette.primary.main
+    },
+
+    appLoading__message: {
+      color: '#fff',
+      marginTop: '1.5em'
+    }
   }
 }
+
+class Loading extends Component {
+  render () {
+    const { classes } = this.props
+    return (
+      <section className={classes.appLoading}>
+        <img src='images/loader.gif' width='40' alt='Loading image' />
+
+        <Typography
+          className={classes.appLoading__message}
+          variant='title'>
+          Loading please wait
+        </Typography>
+      </section>
+    )
+  }
+}
+
+export default withStyles(styles)(Loading)
