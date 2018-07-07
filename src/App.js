26d7b8bca7a344b78a889402601a8297655d62d5
Luca Tabone <taboneluca3@gmail.com>
2018-07-07 08:36:53 +0200

    Refactor App Component
    Update the App component so it includes:
      - App's Bottom Navigation (using BottomNavigation component).
      - App's FAB (using FABContainer component)
      - App's routes:
        - Profile Page (i.e. routed at '/')
        - Calendar Page (i.e. routed at '/calendar')
        - Messages Page (i.e. routed at '/messages')
        - Alerts Page (i.e. routed at '/alerts')

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Route } from 'react-router-dom'
+import BottomNavigation from './BottomNavigation'
+import FABContainer from './FABContainer'
+import AlertsPage from './AlertsPage'
+import CalendarPage from './CalendarPage'
+import MessagesPage from './MessagesPage'
+import ProfilePage from './ProfilePage'
 
-export default class App extends Component {
-  render () {
-    return (<span>App</span>)
+const styles = () => {
+  return {
+    appApp: {
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh'
+    },
+
+    appApp__content: {
+      flex: 1,
+      position: 'relative'
+    }
   }
 }
+
+class App extends Component {
+  render () {
+    const { classes } = this.props
+
+    return (
+      <div className={classes.appApp}>
+        <div className={classes.appApp__content}>
+          <Route exact path='/' component={ProfilePage}/>
+          <Route exact path='/calendar' component={CalendarPage}/>
+          <Route exact path='/messages' component={MessagesPage}/>
+          <Route exact path='/alerts' component={AlertsPage}/>
+
+          <FABContainer />
+        </div>
+
+        <BottomNavigation />
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(App)
